70e84419be1b7989aee60e847f91a7da7e3d30ac
Kaium Al Limon <limon.3cs@gmail.com>
2023-06-12 00:56:02 +0600

    dd

--- conditional-statements.py
+++ conditional-statements.py
@@ -1,5 +1,12 @@
-age=int(input("How old are you? "))
+# age=int(input("How old are you? "))
+#
+# if age>=18:print("You're an adult")
+# elif age<=0:print("You haven't borned yet")
+# else:print("You're not an adult")
 
-if age>=18:print("You're an adult")
-elif age<=0:print("You haven't borned yet")
-else:print("You're not an adult")
+
+# a=19
+# b=12
+#
+# if (not (a>20 and b<10)):print("Available")
+# else: print("unavailable")
---
+++ while-loop.py
@@ -0,0 +1,10 @@
+# while 1:
+#     print("Help")
+
+
+num=int(input("Enter an integer: "))
+
+i=1;
+while(i<=num):
+    print(i)
+    i+=1
